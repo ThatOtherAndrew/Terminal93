9f0962ea48b4db36a47496cad08e3fa57f372f14
Andromeda <stroev.andrew@gmail.com>
2025-12-26 04:33:04 +0000

    Add desktop shortcuts for apps

--- terminal93/app.py
+++ terminal93/app.py
@@ -4,39 +4,34 @@ from typing import TYPE_CHECKING
 
 from textual import work
 from textual.app import App
-from textual.widgets import Static
+from textual.reactive import var
 
 from terminal93.screens.BootScreen import BootScreen
+from terminal93.widgets.desktop import Desktop
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
+
     from terminal93 import Application
 
 
 class Terminal93(App):
-    # language=SCSS
-    CSS = """
-    #background {
-        width: 100vw;
-        height: 100vh;
-        hatch: right $primary-background;
-    }
-    """
+    apps: var[list[Application]] = var([])
 
     def __init__(self) -> None:
         super().__init__()
         self.apps: list[Application] = []
 
     def compose(self) -> ComposeResult:
-        yield Static(id='background')
+        yield Desktop().data_bind(Terminal93.apps)
 
     @work
     async def on_mount(self) -> None:
         from terminal93.apps.counter import Counter
         from terminal93.apps.welcome import Welcome
 
-        self.install_app(Counter)
         self.install_app(Welcome)
+        self.install_app(Counter)
 
         await self.push_screen_wait(BootScreen())
 
@@ -47,3 +42,4 @@ class Terminal93(App):
         app_instance = app(self)
         app_instance.install()
         self.apps.append(app(self))
+        self.mutate_reactive(Terminal93.apps)
--- terminal93/widgets/desktop.py
+++ terminal93/widgets/desktop.py
@@ -1,4 +1,68 @@
-from textual.containers import Grid
+from textual.app import ComposeResult
+from textual.containers import Center, CenterMiddle, Grid
+from textual.reactive import reactive
+from textual.widgets import Label, Placeholder
+
+from terminal93.apps.base import Application
 
 
-class Desktop(Grid): ...
+class DesktopApp(CenterMiddle, can_focus=True):
+    # language=SCSS
+    DEFAULT_CSS = """
+    DesktopApp {
+        &, Center {
+            hatch: right $primary-background;
+        }
+        
+        &:hover {
+            border: $primary-background;
+        }
+        
+        &:focus {
+            border: $primary;
+        }
+    } 
+    
+    Center {
+        width: auto;
+    }
+    
+    Placeholder {
+        width: 6;
+        height: 3;
+        margin-bottom: 1;
+    }
+    """
+
+    def __init__(self, app: Application) -> None:
+        super().__init__()
+        self.target_app = app
+
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield Placeholder()
+        with Center():
+            yield Label(self.target_app.NAME)
+
+
+class Desktop(Grid):
+    # language=SCSS
+    DEFAULT_CSS = """
+    Desktop {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        padding: 1 2;
+
+        grid-gutter: 1;
+        grid-rows: 7;
+        grid-columns: 12;
+        
+        hatch: right $primary-background;
+    } 
+    """
+
+    apps: reactive[list[Application]] = reactive([], recompose=True)
+
+    def compose(self) -> ComposeResult:
+        yield from map(DesktopApp, self.apps)
