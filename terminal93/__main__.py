f107e1cb0f191772b500b66ab88d092b5f5656cc
Andromeda <stroev.andrew@gmail.com>
2025-12-28 05:52:39 +0000

    Add textual-serve

--- terminal93/__main__.py
+++ terminal93/__main__.py
@@ -1,8 +1,15 @@
+import sys
+
+from textual_serve.server import Server
+
 from terminal93 import Terminal93
 
 
 def main() -> None:
-    Terminal93().run()
+    if '--serve' in sys.argv:
+        Server('terminal93', host='0.0.0.0').serve()
+    else:
+        Terminal93().run()
 
 
 if __name__ == '__main__':
