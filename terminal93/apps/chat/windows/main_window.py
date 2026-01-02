42305094cbb42ec8e94ecab5954f7e912328af0f
Andromeda <stroev.andrew@gmail.com>
2026-01-02 19:53:13 +0000

    Add NSFW chat link filter

--- terminal93/apps/chat/windows/main_window.py
+++ terminal93/apps/chat/windows/main_window.py
@@ -1,4 +1,6 @@
 import random
+import re
+from importlib.resources import files
 
 from rich.text import Text
 from textual.app import ComposeResult
@@ -7,6 +9,7 @@ from textual.widgets import RichLog
 
 from terminal93 import Terminal93, Window
 
+from .. import resources
 from ..utils import events
 from ..utils.client import Client
 from ..utils.types import User
@@ -28,6 +31,13 @@ class ChatWindow(Window):
         self.client = Client(self)
         random_nick = f'term93_{random.randint(1, 999):03}'
         self.set_reactive(ChatWindow.nick, random_nick)
+
+        with (files(resources) / 'nsfw_domains.txt').open('r') as file:
+            self.nsfw_domains = {
+                line
+                for line in map(str.strip, file)
+                if line and not line.startswith('#')
+            }
 
     def content(self) -> ComposeResult:
         yield Sidebar().data_bind(ChatWindow.users)
@@ -63,9 +73,17 @@ class ChatWindow(Window):
 
     def on_chat_message(self, event: events.ChatMessage) -> None:
         msg = Text.assemble(
-            event.message.colour_nick(), (': ', 'dim'), event.message.msg
+            event.message.colour_nick(), (': ', 'dim'), self.sanitise(event.message.msg)
         )
         self.query_one(RichLog).write(msg)
 
     async def connect(self) -> None:
         await self.client.start_connection()
+
+    def sanitise(self, message: str) -> str:
+        for match in re.finditer(r'https?://([^/\s]+)\S*', message):
+            url = match.group()
+            domain = match.group(1)
+            if any(domain.endswith(nsfw) for nsfw in self.nsfw_domains):
+                message = message.replace(url, '[[HYPERLINK BLOCKED]]')
+        return message
